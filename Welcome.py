2a4e0fec5b79aeffa649ef496a7eff05156b66df
Habtamu B <Habtamu.berhe@pursuit.com>
2025-06-15 14:35:54 -0400

    adding the first python and html files

---
+++ Welcome.py
@@ -0,0 +1,8 @@
+# Prompt the user to enter their name
+name = input("Please enter your name: ")
+
+# Create the personalized welcome message
+welcome_message = f"Hello, {name}! Welcome to the world of Python programming."
+
+# Print the message to the console
+print(welcome_message)
